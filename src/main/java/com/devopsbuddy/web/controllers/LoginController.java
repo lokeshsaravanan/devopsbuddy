f57b5e9d9e0c7b6517af9744cc2cb36e22d6297b
Lokesh Ramamoorthi <lokeshsaravanan.r@gmail.com>
2017-09-19 22:59:34 -0400

    Added the payload page:

--- src/main/java/com/devopsbuddy/web/controllers/LoginController.java
+++ src/main/java/com/devopsbuddy/web/controllers/LoginController.java
@@ -1,13 +1,10 @@
-package com.devopsbuddy.web.controllers.advices;
+package com.devopsbuddy.web.controllers;
 
 import com.devopsbuddy.backend.service.EmailService;
-import com.devopsbuddy.web.domain.frontend.FeedbackPojo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
