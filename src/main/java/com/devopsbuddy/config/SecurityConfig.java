ede758e85177fc09518d7567a186bcb81bea051c
Lokesh Ramamoorthi <lokeshsaravanan.r@gmail.com>
2017-09-20 18:29:44 -0400

    Files for login/logout functionality

--- src/main/java/com/devopsbuddy/config/SecurityConfig.java
+++ src/main/java/com/devopsbuddy/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.devopsbuddy.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring4.SpringTemplateEngine;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/contact/**",
             "/error/**/*",
     };
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
